ba40f7f42436f494637ca56ef09623360dffc626
João Victor <joaovictorfernandescastro@gmail.com>
2026-02-18 18:31:21 -0300

    feat: adiciona variáveis de ambiente (POSTGRES_URL, REDIS_URL, WEB_URL)

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,9 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  POSTGRES_URL: z.url(),
+  REDIS_URL: z.url(),
+  WEB_URL: z.url(),
 })
 
 export const env = envSchema.parse(process.env)
